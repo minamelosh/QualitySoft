73f5a3f41dfa735b61d8bb330c00bd49f76e2c3f
minamelosh <minamelosh@gmail.com>
2016-11-30 00:22:02 -0800

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -19,11 +19,46 @@ public class Person implements Comparable<Person> {
 	}
 	
 	/*
-	 * Returns new contacts data from class ContactList.
+	 * Makes private variables available for class Contact list to set the values stored in them.
 	 */
-	public String getInfo(){
-		
-		return "";
+	public void setLastName(String newLastName){
+		lastName = newLastName;
+	}
+	public void setFirstName(String newFirstName){
+		firstName = newFirstName;
+	}
+	public void setAddress(String newAddress){
+		address = newAddress;
+	}
+	public void setEmail(String newEmail){
+		email = newEmail;
+	}
+	public void setPhoneNumber(String newPhoneNumber){
+		phoneNumber = newPhoneNumber;
+	}
+	public void setNotes(String newNotes){
+		notes = newNotes;
+	/**
+	 * Allows class person to get the new values stored in private variables. 
+	 */
+	}
+	public String getLastName(){
+		return lastName;
+	}
+	public String getFirstName(){
+		return firstName;
+	}
+	public String getAddress(){
+		return address;
+	}
+	public String getEmail(){
+		return email;
+	}
+	public String getPhoneNumber(){
+		return phoneNumber;
+	}
+	public String getNotes(){
+		return notes;
 	}
 	
 	/**
