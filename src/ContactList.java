f87e6352a72d2e5a3038849f4fdb8d9cf5b665b0
Eugene Krivchenko <eu.kriv@gmail.com>
2016-11-30 21:04:19 -0800

    Fixes for Use case 1

--- src/ContactList.java
+++ src/ContactList.java
@@ -3,28 +3,33 @@
  * a list of contacts.  Class contactList allows you 
  * to create a new contact, search for a contact by 
  * last name, print the entire list in alphabetical 
- * order by last name, and save to desk.
+ * order by last name, and save to disk.
  * @ author MM
  */
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.*;
 public class ContactList {
 	/**
 	 * Creates an array of object persons.
 	 */
 	private ArrayList<Person> contactList = new ArrayList<Person>();
-	
 	/**
 	 * Returns true if user input for the new contact has all the 
 	 * necessary info and adds the new contact to the contact list.  
 	 * If missing last name than returns false and the contact is
 	 * not added to the contact list. 
 	 */
-	public boolean createContact(){
+	public void createContact(){
 	Scanner console = new Scanner (System.in);
 		System.out.println("Please fill out the following.");
 		System.out.print("Last Name: ");
-		String lastName = console.nextLine();
+		String lastName = console.nextLine();		
+		if (lastName == null || lastName.length() == 0){  								//put check here! no reason to execute further code if it fails
+			System.out.println("Last name required. Your contact was not created.");
+			console.close();
+			return;
+		}else {	
 		System.out.print("First Name: ");
 		String firstName = console.nextLine();
 		System.out.print("Address: ");
@@ -35,13 +40,12 @@ public class ContactList {
 		String phoneNumber= console.nextLine();
 		System.out.print("Notes: ");
 		String notes = console.nextLine();
-		if (lastName == null || lastName.length() == 0){
-			System.out.println("Last name required. Your contact was not saved.");
-			return false;
-		}
 		Person contact = new Person(lastName, firstName, address, email, phoneNumber, notes);
-		return true;
-	}
+		System.out.println(contact.toString());
+		System.out.println("Contact has been created successfully!");
+		console.close();
+			}
+		}
 	 
 	/**
 	 * Prints out whole contact list in alphabetical order 
---
+++ src/app.java
@@ -0,0 +1,15 @@
+/*
+ * Contact List application
+ */
+public class app {
+
+	public static void main(String[] args) {
+ContactList app = new ContactList();
+app.createContact();
+
+System.out.println("Use case #1 implementation done!");
+		
+		
+	}
+
+}
