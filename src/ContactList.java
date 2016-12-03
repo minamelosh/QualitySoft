c405e6486924fecb834be537e8bef1aa535f9fc0
John Loughrin <johnmloughrin@gmail.com>
2016-12-03 14:41:10 -0800

    Run autoformatter, import project into eclipse.

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,3 +1,4 @@
+
 /*
  * Creates reusable Class contactList, which stores 
  * a list of contacts.  Class contactList allows you 
@@ -8,77 +9,78 @@
  */
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.*;
+
 public class ContactList {
 	/**
 	 * Creates an array of object persons.
 	 */
 	private ArrayList<Person> contactList = new ArrayList<Person>();
+
 	/**
-	 * Returns true if user input for the new contact has all the 
-	 * necessary info and adds the new contact to the contact list.  
-	 * If missing last name than returns false and the contact is
-	 * not added to the contact list. 
+	 * Returns true if user input for the new contact has all the necessary info
+	 * and adds the new contact to the contact list. If missing last name than
+	 * returns false and the contact is not added to the contact list.
 	 */
-	public void createContact(){
-	Scanner console = new Scanner (System.in);
+
+	public void createContact() {
+
+		Scanner console = new Scanner(System.in);
 		System.out.println("Please fill out the following.");
 		System.out.print("Last Name: ");
-		String lastName = console.nextLine();		
-		if (lastName == null || lastName.length() == 0){  								//put check here! no reason to execute further code if it fails
+		String lastName = console.nextLine();
+		if (lastName == null || lastName.length() == 0) {
+			// put check here! no reason to execute further code if it fails
 			System.out.println("Last name required. Your contact was not created.");
 			console.close();
 			return;
-		}else {	
-		System.out.print("First Name: ");
-		String firstName = console.nextLine();
-		System.out.print("Address: ");
-		String address = console.nextLine();
-		System.out.print("Email Address: ");
-		String email = console.nextLine();
-		System.out.print("Phone Number: ");
-		String phoneNumber= console.nextLine();
-		System.out.print("Notes: ");
-		String notes = console.nextLine();
-		Person contact = new Person(lastName, firstName, address, email, phoneNumber, notes);
-		System.out.println(contact.toString());
-		System.out.println("Contact has been created successfully!");
-		console.close();
-			}
+		} else {
+			System.out.print("First Name: ");
+			String firstName = console.nextLine();
+			System.out.print("Address: ");
+			String address = console.nextLine();
+			System.out.print("Email Address: ");
+			String email = console.nextLine();
+			System.out.print("Phone Number: ");
+			String phoneNumber = console.nextLine();
+			System.out.print("Notes: ");
+			String notes = console.nextLine();
+			Person contact = new Person(lastName, firstName, address, email, phoneNumber, notes);
+			System.out.println(contact.toString());
+			System.out.println("Contact has been created successfully!");
+			console.close();
 		}
-	 
-	/**
-	 * Prints out whole contact list in alphabetical order 
-	 * by last name.
-	 */
-	public void printContactList(){
-		//... for loop and sort method
-		//uses method compareTo(person) from class person
 	}
-	
+
 	/**
-	 * Returns all contacts with last names equal to the one
-	 * we are searching for.  If there are no matches in the 
-	 * contact list this will return a message stating this 
-	 * contact doesn't exist.
+	 * Prints out whole contact list in alphabetical order by last name.
 	 */
-	public Person getContact(String lastName){
-		//uses method equals(lastname) from class person
-		
+	public void printContactList() {
+		// ... for loop and sort method
+		// uses method compareTo(person) from class person
+	}
+
+	/**
+	 * Returns all contacts with last names equal to the one we are searching
+	 * for. If there are no matches in the contact list this will return a
+	 * message stating this contact doesn't exist.
+	 */
+	public Person getContact(String lastName) {
+		// uses method equals(lastname) from class person
+
 		return null;
 	}
-	
+
 	/**
 	 * Saves the ContactList to disk.
 	 */
-	public void save(){
-		
+	public void save() {
+
 	}
-	
+
 	/**
 	 * Loads the saved ContactList from a file.
 	 */
-	public void load(){
-		
+	public void load() {
+
 	}
 }
